cffc13c939ffa4ccabb9a3d6fa3f2115c9ced8b1
Alan Kehoe <alankehoe111@gmail.com>
2015-03-01 13:04:30 +0000

    forgery, remove stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
-  # protect_from_forgery with: :null_session, if: proc { |c| c.request.format == 'application/json' }
+  protect_from_forgery with: :exception
 
   def authenticate_admin_user!
-    raise SecurityError unless current_user.try(:admin?)
-  end
-
-  rescue_from SecurityError do |exception|
-    reset_session
-    redirect_to '/admin/login'
-  end
-  
-  def self.example_file(resources, action, format)
-    File.read("#{Rails.root}/public/docs/#{resources.join('/')}/#{action}.#{format}") rescue ''
+    unless current_user && current_user.try(:admin?)
+      reset_session
+      redirect_to '/admin/login'
+    end
   end
 end
