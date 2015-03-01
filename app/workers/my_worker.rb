36ecd3b9ea264b2aa2a9b1e87be7f9f3d6fcb405
Alan Kehoe <alankehoe111@gmail.com>
2015-03-01 12:31:56 +0000

    fix worker

--- app/workers/my_worker.rb
+++ app/workers/my_worker.rb
@@ -7,10 +7,6 @@ class MyWorker
   sidekiq_options queue: :schedueled
 
   def perform
-    stat = DiagnosticStatistic.new rpm: 1235,
-                                   speed: 123.0,
-                                   consumption: 5.5,
-                                   car_id: 1
-    stat.save!
+
   end
 end
